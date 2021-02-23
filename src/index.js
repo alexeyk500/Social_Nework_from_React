0b14aded4b830b9a8e32e74299d7346cdf8c96fd
Алексей Стюпан <alexeyk500@yandex.ru>
2021-02-23 22:53:34 +0300

    store done

--- src/index.js
+++ src/index.js
@@ -1,4 +1,36 @@
-import state from './redux/state';
-import {renderDOM} from './render'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter} from 'react-router-dom';
 
-renderDOM(state);
+import App from './App';
+
+import store from './redux/state';
+
+
+let reRenderDOM = (store) => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <BrowserRouter>
+        <App state={store.getState()}
+             addPost={store.addPost.bind(store)}
+             preSavePost={store.preSavePost.bind(store)}/>
+      </BrowserRouter>
+    </React.StrictMode>, document.getElementById('root')
+  );
+};
+
+// let reRenderDOM = (store) => {
+//   ReactDOM.render(
+//     <React.StrictMode>
+//       <BrowserRouter>
+//         <App store={store}/>
+//       </BrowserRouter>
+//     </React.StrictMode>, document.getElementById('root')
+//   );
+// };
+
+store.subscribe(reRenderDOM);
+
+reRenderDOM(store);
+
+
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,41 +1,54 @@
-import {renderDOM} from './../render';
-
-let state = {
-  profilePage: {
-    posts: [{postId:1, postText:'Hi, how are you?'},
-            {postId:2, postText:'Do you listen to me?'},
-            {postId:3, postText:'Why do you not answer?'},
-            {postId:4, postText:'blablabla - blablabla'},
-            ],
-    textNewPost: '',
-  },
-
-  dialogsPage :{
-    dialogs: [{userId:1, userName:'Olga'},
-              {userId:2, userName:'Ivan'},
-              {userId:3, userName:'Kati'},
-              {userId:4, userName:'Ange'},
-              {userId:5, userName:'Iveya'},
-              {userId:6, userName:'Stepan'},
-              {userId:7, userName:'My'},
-             ],
-    messages: [{messageId:1, messagetext:'Hi'},
-               {messageId:2, messagetext:'How is your day?'},
-               {messageId:3, messagetext:'Will you come today?'},
+let store = {
+  _state: {
+    profilePage: {
+      posts: [{postId:1, postText:'Hi, how are you?'},
+              {postId:2, postText:'Do you listen to me?'},
+              {postId:3, postText:'Why do you not answer?'},
+              {postId:4, postText:'blablabla - blablabla'},
               ],
+      textNewPost: '',
+    },
+
+    dialogsPage :{
+      dialogs: [{userId:1, userName:'Olga'},
+                {userId:2, userName:'Ivan'},
+                {userId:3, userName:'Kati'},
+                {userId:4, userName:'Ange'},
+                {userId:5, userName:'Iveya'},
+                {userId:6, userName:'Stepan'},
+                {userId:7, userName:'My'},
+               ],
+      messages: [{messageId:1, messagetext:'Hi'},
+                 {messageId:2, messagetext:'How is your day?'},
+                 {messageId:3, messagetext:'Will you come today?'},
+                ],
+    },
   },
+
+  getState() {
+    return this._state;
+  },
+
+  addPost() {
+    let newPost = {postId:5, postText:this._state.profilePage.textNewPost};
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.textNewPost = '';
+    this.reRenderDOM(this);
+  },
+
+  preSavePost(text){
+    this._state.profilePage.textNewPost = text;
+    this.reRenderDOM(this);
+  },
+
+  reRenderDOM() {
+    console.log('state has been changed');
+  },
+
+  subscribe(observer){
+    this.reRenderDOM = observer;
+  },
+
 };
 
-export let addPost = (postMessage) => {
-  let newPost = {postId:5, postText:postMessage};
-  state.profilePage.posts.push(newPost);
-  state.profilePage.textNewPost = '';
-  renderDOM(state);
-};
-
-export let preSavePost = (text) => {
-  state.profilePage.textNewPost = text;
-  renderDOM(state);
-}
-
-export default state;
+export default store;
